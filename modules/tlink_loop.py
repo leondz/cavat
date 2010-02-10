4ffbf55981ec373bbce5fc388ea95e7065ba0280
leon <leon@dcs.shef.ac.uk>
2010-02-10 18:37:00 +0000

    added event loop check to tlink_loop

--- modules/tlink_loop.py
+++ modules/tlink_loop.py
@@ -12,6 +12,26 @@ class tlink_loop(CavatModule):
     
     _minVersion = 0.1
     _maxVersion = 0.999
+
+
+
+    def uniq(self, seq, idfun=None):  
+        
+        # order preserving 
+        if idfun is None: 
+            def idfun(x): return x 
+        seen = {} 
+        result = [] 
+        for item in seq: 
+            marker = idfun(item) 
+            # in old Python versions: 
+            # if seen.has_key(marker) 
+            # but in new ones: 
+            if marker in seen: continue 
+            seen[marker] = 1 
+            result.append(item) 
+        return result
+
     
     def checkDocument(self,  doc_id):
         
@@ -30,22 +50,23 @@ class tlink_loop(CavatModule):
         loopedTlinks = []
 
         # look at where we're linking an event instance to itself
-        if not runQuery('SELECT lid,relType, arg1 FROM tlinks WHERE arg1 = arg2 AND doc_id = ' + doc_id + ' ORDER BY CAST(SUBSTRING(lid,2) AS SIGNED)'):
+        if not runQuery('SELECT lid, relType, arg1, arg2 FROM tlinks WHERE arg1 = arg2 AND doc_id = ' + doc_id + ' ORDER BY CAST(SUBSTRING(lid,2) AS SIGNED)'):
             return
         
         loopedTlinks = db.cursor.fetchall()
         
-        
         # look at where we're linking an event instance to itself
-        if not runQuery('SELECT lid,relType, arg1 FROM tlinks WHERE arg1 = arg2 AND doc_id = ' + doc_id + ' ORDER BY CAST(SUBSTRING(lid,2) AS SIGNED)'):
+        if not runQuery('SELECT lid, reltype, arg1, arg2 FROM tlinks AS t, instances AS i1, instances AS i2 WHERE t.arg1 = i1.eiid AND t.arg2 = i2.eiid AND t.doc_id = i1.doc_id AND t.doc_id = i2.doc_id AND i1.eventID = i2.eventID AND t.doc_id = ' + doc_id + ' ORDER BY CAST(SUBSTRING(lid,2) AS SIGNED)'):
             return
         
+        loopedTlinks = self.uniq(loopedTlinks + db.cursor.fetchall())
+        
+        
         if loopedTlinks:
-            
             print "# Checking " + docName + ' (id ' + doc_id + ')'
             
             for row in loopedTlinks: 
-                print 'TLINK ID %s matches, type %s, event %s' % (row[0],  row[1],  row[2])
+                print 'TLINK ID %s matches, type %s, event %s / %s' % (row[0],  row[1],  row[2],  row[3])
         else:
             if cavatDebug.debug:
                 print 'No looping TLINKs found in this document.'
